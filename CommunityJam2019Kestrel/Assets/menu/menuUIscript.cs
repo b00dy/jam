854e0cc78cca8925a34391cb53ad2adbb3bd0d72
abhinandan-lab <52070972+abhinandan-lab@users.noreply.github.com>
2019-08-27 18:03:50 +0530

    menu UI

---
+++ CommunityJam2019Kestrel/Assets/menu/menuUIscript.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class menuUIscript : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    Resolution[] resolutions;
+    public Dropdown resolutionDropdown;
+
+
+   public void onPlay()
+    {
+        // when player press on play button in main-menu option
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // scene will transist to next scene in builindex...
+    }
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " X " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && 
+                resolutions[i].height == Screen.currentResolution.height)
+                currentResolutionIndex = i;
+        }
+            
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void onQuit()
+    {
+        Application.Quit();
+        Debug.Log("QUIT!...");
+    }
+
+    public void setVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void setGraphicsQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void setFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void setResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+
+}
---
+++ CommunityJam2019Kestrel/Assets/menu/pauseMenu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                resume();
+            }
+            else
+            {
+                pause();
+            }
+
+        }
+        
+    }
+
+    public void resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+    void pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void loadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("menuUI");
+    }
+
+    public void onQuit()
+    {
+        Application.Quit();
+        Debug.Log("Quit!...");
+    }
+
+    public void onRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
